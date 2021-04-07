fa4321645e153511dd5642b204d66efd61b4a863
TejasRangole1 <rangole@wisc.edu>
2021-04-06 17:35:41 -0700

    startConnection() v2

--- Network.java
+++ Network.java
@@ -84,7 +84,13 @@ public class Network {
         outStream.writeShort(checksum);
         outStream.write(data);
         byte[] packetData = byteStream.toByteArray();
-        DatagramPacket outgoingPacket = new DatagramPacket(packetData, packetData.length, remoteIP, port);
+        DatagramPacket outgoingPacket = null;
+        if(remoteIP == null){
+            outgoingPacket = new DatagramPacket(packetData, packetData.length);
+        }
+        else {
+            new DatagramPacket(packetData, packetData.length, remoteIP, port);
+        }
         Segment segment = new Segment(outgoingPacket, seqNum, timestamp);
         System.out.println( "FROM: " + whoami +  " Network.java: sendSegment(): SENT SYN= " + seqNum + " SENT ACK= " + ack);
         if(whoami.equals("Sender")){
