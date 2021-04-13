c1bb22820c92510bf971cf3c00ee989d9fc62538
TejasRangole1 <rangole@wisc.edu>
2021-04-13 16:10:07 -0700

    debugging length extraction

--- TCPend.java
+++ TCPend.java
@@ -8,7 +8,8 @@ public class TCPend {
             int remotePort = Integer.parseInt(args[5]);
             int mtu = Integer.parseInt(args[9]);
             int sws = Integer.parseInt(args[11]);
-            Sender sender = new Sender(port, remotePort, remoteIP, mtu, sws);
+            String filename = args[7];
+            Sender sender = new Sender(port, remotePort, remoteIP, mtu, sws, filename);
         }        
         else{
             // Receiver
